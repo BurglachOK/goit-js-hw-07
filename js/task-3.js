d6c40d9b56315d03c2efa092cf88dfb18f7cfb93
Serhii <158744795+BurglachOK@users.noreply.github.com>
2024-06-07 08:14:05 +0200

    3 out of 6 tasks

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+const inputNameElem = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputNameElem.addEventListener('input', oninputNameInput);
+function oninputNameInput() {
+
+    const inputValue = inputNameElem.value.trim();
+
+    if (inputValue) {
+        outputName.textContent = inputValue;
+    } else {
+        outputName.textContent = 'Anonymous';
+    }
+}
